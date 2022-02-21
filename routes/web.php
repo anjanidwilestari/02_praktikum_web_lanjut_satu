9f5036c3f9273684286f4319d1b254e8080b7e76
Ayi <ayianjani774@gmail.com>
2022-02-21 09:50:18 +0700

    Kode Program Pertama

---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return "Selamat Datang";
+// });
+// Route::get('/', function () {
+//     echo "Selamat Datang";
+// });
+// Route::get('/about', function () {
+//     echo "2041720180 Anjani Dwilestari";
+// });
+// Route::get('/articles/{id}', function ($id) {
+//     echo "Halaman Artikel dengan ID $id";
+// });
+Route::get('/articles/1', function ($id=1) {
+    echo "Halaman Artikel dengan ID $id";
+});
+Route::get('/articles/2', function ($id=2) {
+    echo "Halaman Artikel dengan ID $id";
+});
